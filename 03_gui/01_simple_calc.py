21d02c641e9a666a5602d823a85c97906f71f20b
mtszc <mtszccr@gmail.com>
2022-04-20 13:56:24 +0200

    Add solution to 3.1.1.2 LAB GUI

---
+++ 03_gui/01_simple_calc.py
@@ -0,0 +1,79 @@
+"""
+3.1.1.2 LAB: A very simple calculator
+https://edube.org/learn/pcpp1-4-gui-programming/lab-a-very-simple-calculator
+"""
+import tkinter as tk
+from tkinter import messagebox
+
+
+def set_string_1(*args):
+    global last_string_1
+    string = text_1.get()
+    last_string_1 = string
+
+
+def set_string_2(*args):
+    global last_string_2
+    string = text_2.get()
+    last_string_2 = string
+
+
+def evaluate(*args):
+    global last_string_1
+    global last_string_2
+    input_error_flag = 0
+    try:
+        left_operand = float(last_string_1)
+        input_error_flag = 1
+        right_operand = float(last_string_2)
+        if switch.get() == 0:
+            result = left_operand + right_operand
+            messagebox.showinfo(title='Result', message=f'{result}')
+        elif switch.get() == 1:
+            result = left_operand - right_operand
+            messagebox.showinfo(title='Result', message=f'{result}')
+        elif switch.get() == 2:
+            result = left_operand * right_operand
+            messagebox.showinfo(title='Result', message=f'{result}')
+        elif switch.get() == 3:
+            try:
+                result = left_operand / right_operand
+                messagebox.showinfo(title='Result', message=f'{result}')
+            except ZeroDivisionError:
+                entry_2.focus_set()
+                messagebox.showerror(title='Result', message='ZeroDivisionError')
+    except ValueError:
+        if input_error_flag == 0:
+            entry_1.focus_set()
+            messagebox.showerror(title='Result', message="Wrong type of left operand.")
+        else:
+            entry_2.focus_set()
+            messagebox.showerror(title='Result', message="Wrong type of right operand.")
+
+
+window = tk.Tk()
+last_string_1 = ''
+last_string_2 = ''
+text_1 = tk.StringVar()
+text_2 = tk.StringVar()
+text_1.set(last_string_1)
+text_1.trace('w', set_string_1)
+text_2.set(last_string_2)
+text_2.trace('w', set_string_2)
+entry_1 = tk.Entry(window, width=20, textvariable=text_1)
+entry_2 = tk.Entry(window, width=20, textvariable=text_2)
+button_1 = tk.Button(window, text="Evaluate", command=evaluate)
+switch = tk.IntVar()
+switch.set(0)
+radiobutton_1 = tk.Radiobutton(window, text="+", width=5, variable=switch, value=0)
+radiobutton_2 = tk.Radiobutton(window, text="-", width=5, variable=switch, value=1)
+radiobutton_3 = tk.Radiobutton(window, text="*", width=5, variable=switch, value=2)
+radiobutton_4 = tk.Radiobutton(window, text="/", width=5, variable=switch, value=3)
+button_1.grid(row=4, column=1)
+radiobutton_1.grid(row=0, column=1)
+radiobutton_2.grid(row=1, column=1)
+radiobutton_3.grid(row=2, column=1)
+radiobutton_4.grid(row=3, column=1)
+entry_1.grid(row=2, column=0)
+entry_2.grid(row=2, column=2)
+window.mainloop()
